80caf15b93d89b4d6ba339e255ca9b8073ff6125
QuasimRizvi <quasi.rizvi@gmail.com>
2023-09-03 21:26:12 -0500

    Module Commit: Module 1 Commit

--- src/main/java/com/spb3/module1/firstapp/rest/FirstRestController.java
+++ src/main/java/com/spb3/module1/firstapp/rest/FirstRestController.java
@@ -1,10 +1,17 @@
 package com.spb3.module1.firstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstRestController {
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${player.name}")
+    private String playerName;
 
     @GetMapping("/")
     public String sayHello(){
@@ -14,5 +21,9 @@ public class FirstRestController {
     @GetMapping("/devtools")
     public String demoDevTools(){
         return "Auto Reloading Devtools";
+    }
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return playerName+" plays for: "+teamName;
     }
 }
